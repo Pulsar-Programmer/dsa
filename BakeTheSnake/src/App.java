d032043539aa37e297e71ed08f653edf75fb2110
PulsarProgrammer <programingcoder101@gmail.com>
2024-10-04 10:33:51 -0400

    Add snake components

--- BakeTheSnake/src/App.java
+++ BakeTheSnake/src/App.java
@@ -1,11 +1,21 @@
 public class App {
 
-    final static String WALL = "🟦";
-    final static String APPLE = "🟥";
-    final static String EMPTY = "⬛️";
-    final static String SNAKE = "🟩";
+    // final static String APPLE = "🟥";
+    final static char WALL = '#'; //🟦
+    final static char EMPTY = '.'; //⬛️
+    final static char SNAKE = '■'; //🟩
+    final static char LEFT = '<';
+    final static char RIGHT = '>';
+    final static char UP = '^';
+    final static char DOWN = 'v';
 
     public static void main(String[] args) throws Exception {
+        System.out.println(UP);
+        System.out.println(DOWN);
+        System.out.println(LEFT);
+        System.out.println(RIGHT);
+        System.out.println(SNAKE);
         System.out.println(WALL);
+        System.out.println(EMPTY);
     }
 }
--- BakeTheSnake/src/Snake.java
+++ BakeTheSnake/src/Snake.java
@@ -1,15 +1,28 @@
 import java.awt.Point;
 
 public class Snake {
-    private Direction direction;
+
+
+
+
+    private int direction;
     private Point head;
     private Body next;
+    private char[][] board;
 
-    public Direction getDirection() {
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public int getDirection() {
         return direction;
     }
 
-    public void setDirection(Direction direction) {
+    public void setDirection(int direction) {
         this.direction = direction;
     }
 
@@ -30,52 +43,108 @@ public class Snake {
     }
 
     public Snake(){
-        direction = Direction.Down;
+        direction = 3;
         head = new Point(4, 4);
         next = new Body(new Point(4, 3), new Body(4, 2));
+        //TODO fill board
     }
 
-    public Snake(Direction direction, Point head, Body next) {
+    public Snake(int direction, Point head, Body next) {
         this.direction = direction;
         this.head = head;
         this.next = next;
+        //TODO fill board
+    }
+
+    public Snake(int dir, int row, int col, Body p){
+        this.direction = dir;
+        this.head = new Point(row, col);
+        this.next = p;
     }
 
     ///Returns if the snake is alive or not after the command.
     boolean update(char command){
         switch(command) {
-            case 'M' -> {
-                
-            }
-            case 'U' -> {
-                
-            }
-            case 'D' -> {
-                
-            }
-            case 'L' -> {
-                
-            }
-            case 'R' -> {
-                
-            }
-            case 'F' -> {
+            case 'U' : {
+                direction = 1;
+            } break;
+            case 'D' : {
+                direction = 3;
+            } break;
+            case 'L' : {
+                direction = 2;
+            } break;
+            case 'R' : {
+                direction = 0;          
+            } break;
 
-            }
-            case 'E' -> {
 
-            }
+
+
+
+
+
+
+
+
+
+            case 'M' : {
+                final Point next_pos = advanced(head, direction);
+                final boolean found_wall = 
+                    next_pos.x <= -1 || 
+                    next_pos.x >= 10||
+                    next_pos.y <= -1||
+                    next_pos.y >= 10 ;
+                final boolean found_snake = 
+                    next_pos
+
+            } break;
+            case 'F' : {
+
+            } break;
+            case 'E' : {
+
+            } break;
         }
         //TODO
         return false;
     }
 
+    // private char direction_char() {
+    //     return switch(direction){
+    //         case 0 -> {
+    //             yield App.RIGHT;
+    //         },
+    //         case 1 -> {
+    //             yield App.UP;
+    //         },
+    //         case 2 -> {
+    //             yield App.LEFT;
+    //         },
+    //         case 3 -> {
+    //             yield App.DOWN;
+    //         },
+    //     };
+    // }
 
-}
+    private static Point advanced(Point current, int direction){
+        //     return switch(direction){
+        //     case 0 -> {
+        //         yield App.RIGHT;
+        //     },
+        //     case 1 -> {
+        //         yield App.UP;
+        //     },
+        //     case 2 -> {
+        //         yield App.LEFT;
+        //     },
+        //     case 3 -> {
+        //         yield App.DOWN;
+        //     },
+        // };
+        //TODO
+        return null;
+    }
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right;
+
 }
