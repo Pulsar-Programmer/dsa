4cf5c312253e6c3e0c1b87041b143dbae707fcda
PulsarProgrammer <programingcoder101@gmail.com>
2024-10-10 09:57:56 -0400

    Add a SinglyLinkedList

--- customvec/src/main.rs
+++ customvec/src/main.rs
@@ -1,4 +1,4 @@
-use std::{mem::{ManuallyDrop, MaybeUninit}, ops::Deref, rc::{Rc, Weak}};
+use std::{collections::LinkedList, mem::{ManuallyDrop, MaybeUninit}, ops::Deref, rc::{Rc, Weak}};
 
 fn main() {
     println!("Hello, world!");
@@ -61,23 +61,104 @@ struct Node<T>{
 }
 
 
-struct RCLinkedList<T>{
-    head: Next<Node<T>>,
-}
-impl<T> RCLinkedList<T>{
-    ///Returns a reference to the `nth` node in the linked list. Returns the last element if it is `n` is too large,
-    fn nth(&self, n: usize) -> Weak<RCLinkedList<T>>{
-        let mut count = 0;
-        let mut runner = self.head;
+// struct RCLinkedList<T>{
+//     head: Next<Node<T>>,
+// }
+// impl<T> RCLinkedList<T>{
+//     ///Returns a reference to the `nth` node in the linked list. Returns the last element if it is `n` is too large,
+//     fn nth(&self, n: usize) -> Weak<RCLinkedList<T>>{
+//         let mut count = 0;
+//         let mut runner = self.head;
 
-        loop{
-            if count == n {
-                return runner;
-            }
-            let Some(val) = runner.next else { break };
+//         loop{
+//             if count == n {
+//                 return runner;
+//             }
+//             let Some(val) = runner.next else { break };
             
-            count += 1;
-        }
-        runner
+//             count += 1;
+//         }
+//         runner
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+struct SinglyLinkedList<'a, T>{
+    head: Option<&'a SinglyLinkedListNode<'a, T>>
+}
+impl<T> SinglyLinkedList<'_, T>{
+    fn new() -> Self{
+        Self { head: None }
     }
+    
+    ///
+    #[doc(alias("size", "length"))]
+    fn len(&self) -> usize{
+        match self.head{
+            Some(v) => v.len(),
+            None => 0,
+        }
+    }
+    fn is_empty(&self) -> bool{
+        self.head.is_none()
+    }
+
+
+    // fn push(&mut self, val: T){
+
+    // }
+
+    
+}
+
+
+
+
+
+struct SinglyLinkedListNode<'a, T>{
+    data: T,
+    next: Option<&'a SinglyLinkedListNode<'a, T>>,
+}
+impl<T> SinglyLinkedListNode<'_, T>{
+    fn with_data(t: T) -> Self{
+        Self { data: t, next: None }
+    }
+    fn len(&self) -> usize{
+        let mut counter = 0;
+        let mut reference = self;
+        while let Some(v) = reference.next{
+            reference = v;
+            counter += 1;
+        }
+        counter
+    }
+}
+// impl<'a, T> Iterator for SinglyLinkedListNode<'a, T>{
+//     type Item = &'a mut T;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let mut reference = self;
+//         while let Some(v) = reference.next{
+//             reference = v;
+//         }
+//         reference
+//     }
+// }
+
+
+
+#[derive(Default)]
+struct StackLinkedList<T>{
+    inner: LinkedList<T>,
+}
+impl<T> StackLinkedList<T>{
+    
 }
