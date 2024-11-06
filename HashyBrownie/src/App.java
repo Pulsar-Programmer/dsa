c12d5e29b45c0c0f09cf43edc5aa8f96b319ee90
PulsarProgrammer <programingcoder101@gmail.com>
2024-11-06 10:31:53 -0500

    initiate new lab

--- HashyBrownie/src/App.java
+++ HashyBrownie/src/App.java
@@ -1,8 +1,20 @@
-
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+
+
+        var map = new HashMap<String, HashSet<String>>();
+        for(String key : map.keySet()){
+            map.get(key).remove("Apple");
+        }
+        for(HashSet<String> val : map.values()){
+            val.remove("Apple");
+        }
+
         
     }
 }
---
+++ SneakyLink/src/App.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        File file = new File("");
+
+        Scanner scanner = new Scanner(file);
+        
+        
+
+
+    }
+}
