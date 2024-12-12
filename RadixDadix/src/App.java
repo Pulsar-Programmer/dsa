2812b1af906c3a67ec361078e83cc5c7c804b912
PulsarProgrammer <programingcoder101@gmail.com>
2024-12-12 10:33:28 -0500

    Add JFileChooser

--- RadixDadix/src/App.java
+++ RadixDadix/src/App.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        JFileChooser jchoose = new JFileChooser();
+        jchoose.setCurrentDirectory(new File(".\\"));
+        jchoose.showOpenDialog(null);
+
+        File csv = jchoose.getSelectedFile();
+
+        var array = new ArrayList();
+
+        Scanner scanner = new Scanner(csv);
+        
+        scanner.close();
+        
+        JFrame frame = new JFrame();
+        
+        
     }
 }
---
+++ RadixDadix/src/Graphics.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JPanel;
+
+public class Graphics extends JPanel {
+    
+
+    private ArrayList<Piece> pieces;
+
+    public Graphics(ArrayList<Piece> pieces){
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public void MSD
+
+}
