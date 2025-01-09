94750f677b2bdc6be56c349348466b8f132ccf25
PulsarProgrammer <programingcoder101@gmail.com>
2025-01-09 09:20:44 -0500

    Add small changes on DynamicFunction

--- dynamic/src/main.rs
+++ dynamic/src/main.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, collections::HashMap};
+use std::{any::Any, collections::HashMap, hash::Hash};
 
 fn main() {
     println!("Hello, world!");
@@ -6,28 +6,36 @@ fn main() {
 
 
 
+// trait EqHash : Eq + Hash{}
+// // impl EqHash for 
+// struct Val<T>{
 
-
-
-type Arg = (String, Box<dyn Any>);
+// }
+//use serde for String values to convert back into intended type
+type Arg = (String, String);
 struct DynamicFunction<R>{
     method: Box<dyn FnMut(Vec<Arg>) -> R>,
-    args: Vec<Arg>,
+    args: Vec<String>,
     storage: HashMap<Vec<Arg>, R>
 }
 impl<R> DynamicFunction<R>{
     fn new(
         method: Box<dyn FnMut(Vec<Arg>) -> R>,
-        args: Vec<Arg>,
+        args: Vec<String>,
     ) -> Self{
         Self { method, args, storage: HashMap::new() }
     }
 
-    fn access(&self, args: Vec<Arg>) -> R{
-        // if let Some(r) = self.storage
-        todo!()
-    }
-    fn call_and_store(){
-        todo!()
-    }
+    // fn access(&self, args: Vec<Arg>) -> R{
+    //     // if let Some(r) = 
+    //     // self.storage.
+    //     todo!()
+    // }
+
+    // fn call_and_store(&mut self, inp: Vec<String>){
+        
+    //     if let Some(r) = self.storage.get() {
+
+    //     }
+    // }
 }
