e2132d14acbd80549dc4726ed8269050f62cd90d
PulsarProgrammer <programingcoder101@gmail.com>
2025-01-13 10:33:13 -0500

    add a tad to call and store

--- dynamic/src/main.rs
+++ dynamic/src/main.rs
@@ -12,18 +12,18 @@ fn main() {
 
 // }
 //use serde for String values to convert back into intended type
-type Arg = (String, String);
-struct DynamicFunction<R>{
-    method: Box<dyn FnMut(Vec<Arg>) -> R>,
-    args: Vec<String>,
-    storage: HashMap<Vec<Arg>, R>
+// type Arg = (String, String);
+type Value = String;
+struct DynamicFunction<R, const N: usize>{
+    method: Box<dyn FnMut([Value; N]) -> R>,
+    // args: Vec<String>,
+    storage: HashMap<[Value; N], R>
 }
-impl<R> DynamicFunction<R>{
+impl<R, const N: usize> DynamicFunction<R, N>{
     fn new(
-        method: Box<dyn FnMut(Vec<Arg>) -> R>,
-        args: Vec<String>,
+        method: Box<dyn FnMut([Value; N]) -> R>,
     ) -> Self{
-        Self { method, args, storage: HashMap::new() }
+        Self { method, storage: HashMap::new() }
     }
 
     // fn access(&self, args: Vec<Arg>) -> R{
@@ -32,10 +32,14 @@ impl<R> DynamicFunction<R>{
     //     todo!()
     // }
 
-    // fn call_and_store(&mut self, inp: Vec<String>){
-        
-    //     if let Some(r) = self.storage.get() {
+    fn call_and_store(&mut self, inp: [Value; N]) -> &R{
+        if let Some(r) = self.storage.get(&inp) {
+            return r;
+        }
 
-    //     }
-    // }
+        let r = (self.method)(inp);
+        let b = &r;
+        self.storage.insert(inp, r);
+        b
+    }
 }
