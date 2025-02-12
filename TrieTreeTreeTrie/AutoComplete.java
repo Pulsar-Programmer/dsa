23576b31c95c5acefd06dcf8bb5dd719aeb4c9f7
PulsarProgrammer <programingcoder101@gmail.com>
2025-02-12 10:14:04 -0500

    Finished AutoComplete struct

--- TrieTreeTreeTrie/AutoComplete.java
+++ TrieTreeTreeTrie/AutoComplete.java
@@ -1,20 +1,28 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class AutoComplete {
     
-
-
-
-
-
-    public static AutoComplete load_txt(File file){
-        //TODO
-        return new AutoComplete();
+    public static Dictionary load_txt(File file) throws FileNotFoundException{
+        TNode self = new TNode();
+        Scanner scanner = new Scanner(file);
+        scanner.tokens().forEach((t)->{
+            self.addKey(t);
+        });
+        scanner.close();
+        return new Dictionary(self);
     }
 
-    public static AutoComplete load_csv(File file){
-        //TODO
-        return new AutoComplete();
+    public static Dictionary load_csv(File file) throws FileNotFoundException{
+        TNode self = new TNode();
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter(",");
+        scanner.tokens().forEach((t)->{
+            self.addKey(t);
+        });
+        scanner.close();
+        return new Dictionary(self);
     }
 }
--- TrieTreeTreeTrie/src/Dictionary.java
+++ TrieTreeTreeTrie/src/Dictionary.java
@@ -1,3 +1,7 @@
 public class Dictionary {
     private TNode root;
+
+    public Dictionary(TNode root){
+        this.root = root;
+    }
 }
