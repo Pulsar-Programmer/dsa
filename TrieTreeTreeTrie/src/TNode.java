6bb3bbb90cbff909795e18ac3413cc738ef37d72
Pulsar-Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2025-02-15 05:12:59 -0500

    Fixed & Debugged

--- TrieTreeTreeTrie/src/TNode.java
+++ TrieTreeTreeTrie/src/TNode.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
@@ -45,22 +46,30 @@ public class TNode {
 
     /** Deletes an existing Key of the Trie */
     public void deleteKey(String key){
+        ///We create a runner to go to the end and a soonest to keep track of the soonest key/branch before we need to remove others.
         TNode runner = this;
         TNode soonest = this;
-        Character last_c = key.charAt(0);
-        for(Character c : key.toCharArray()){
-            //should it be >1 or ==1?
-            if(runner.next.size() > 1 || runner.isKey){
-                soonest = runner;
-                last_c = c;
+        ///We get the desired node and if it does not exist just return.
+        var desired = getNode(key);
+        if(desired.isEmpty()) return;
+        ///If the node is at the end, then we run to the end and find the last place to put it.
+        ///If the node is not at the end, then we just set it to null simply.
+        if(desired.get().next.isEmpty()){
+            Character last_c = key.charAt(0);
+            for(Character c : key.toCharArray()){
+                if(runner.next.size() > 1 || runner.isKey){
+                    soonest = runner;
+                    last_c = c;
+                }
+                runner = runner.next.get(c.toString());
+                if(runner == null){
+                    return;
+                }
             }
-            runner = runner.next.get(c.toString());
-            if(runner == null){
-                return;
-            }
+            soonest.next.remove(last_c.toString());
+        } else {
+            desired.get().isKey = false;
         }
-        soonest.next.remove(last_c.toString());
-        // runner.isKey = false;
     }
 
     /** Gets a Node or Creates it if it Doesn't Exist */
@@ -77,10 +86,10 @@ public class TNode {
     /** Compresses the tree into perfect Strings by taking any one-node characters and merging them to one unit. */
     public void compress(){
         if(next.size() == 1){
-            var next_1 = next.values().iterator().next();
-            var next_2hm = next_1.next;
-            value += next_1.value;
-            next = next_2hm;
+            var next_next = next.values().iterator().next();
+            next_next.compress();
+            value = value.concat(next_next.value);
+            next = next_next.next;
             compress();
         } else {
             for(TNode n : next.values()){
@@ -89,28 +98,47 @@ public class TNode {
         }
     }
 
+    // /** Searches for all possible Keys based on the first starter words. */
+    // public ArrayList<String> auto(String given){
+    //     ArrayList<String> words = new ArrayList<>();
+    //     var val = getNode(given);
+    //     if(val.isEmpty()){
+
+    //         return words;
+    //     }
+    //     val.get().next.values().forEach((node) -> {
+    //         for (String next_char : node.next.keySet()) {
+    //             for (String composed : auto(given + next_char)) {
+    //                 words.add(given + next_char + composed);
+    //             }
+    //         }
+    //     });
+    //     return words;
+    // }
+
     /** Searches for all possible Keys based on the first starter words. */
     public ArrayList<String> auto(String given){
         ArrayList<String> words = new ArrayList<>();
         var val = getNode(given);
         if(val.isEmpty()){
+            words.add("");
             return words;
         }
-        val.get().next.values().forEach((node) -> {
-            words.addAll(node.auto("").stream().map((word) -> given + word).toList());
-        });  
+        for (var next_node : val.get().next.entrySet()) {
+            var next_char = next_node.getKey();
+            if(next_node.getValue().isKey){
+                words.add(given + next_char);
+            }
+            for (String composed : next_node.getValue().auto("")) {
+                words.add(given + next_char + composed);
+            }
+        }
         return words;
     }
 
     @Override
     public String toString() {
-        String total = new String();
-        total += value + " -> ";
-        for(TNode n : next.values()){
-            total += n.toString() + " \n";
-        }
-
-        return value+"->"+next;
+        return value + "->" + next;
     }
 
 
