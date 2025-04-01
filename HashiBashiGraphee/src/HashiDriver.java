6aae868cc6145fac77e2cdf7f6392bd8fe263093
PulsarProgrammer <programingcoder101@gmail.com>
2025-04-01 10:10:58 -0400

    Instantiate new lab and work some stuff

---
+++ HashiBashiGraphee/src/HashiDriver.java
@@ -0,0 +1,47 @@
+import java.nio.file.Path;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+public class HashiDriver {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) { 
+            System.out.println("Would you like to insert a graph or break?");
+            System.out.println("1. Insert a Graph");
+            System.out.println("2. Break");
+            if(scanner.nextLine().equals("1")){
+                System.out.println("Please select a file to insert a graph from.");
+                Path path = select_file();
+                System.out.println("Inserting graph from " + path.toString() + "...");
+
+                // Insert the graph here using the path
+                // Graph graph = new Graph(path);
+                // graph.insertGraph(path);
+
+            } else {
+                break;
+            }
+
+        }
+
+
+
+
+        scanner.close();
+    }
+
+    /** Selects a file using the java file chooser. */
+    public static Path select_file() throws Exception {
+        // SwingUtilities.invokeLater(() -> {
+            
+        // });
+
+        JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+        if(fileChooser.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
+            throw new Exception("No file selected");
+        }
+        return fileChooser.getSelectedFile().toPath();
+    }
+}
